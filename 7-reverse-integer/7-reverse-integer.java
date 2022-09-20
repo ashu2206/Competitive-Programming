5019d75d5d17be9d89307f193148000b7dc4a26e
Ashu Baghel <102874744+ashu2206@users.noreply.github.com>
2022-09-20 10:49:04 +0530

    Time: 2 ms (79.30%), Space: 40.9 MB (78.90%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -15,17 +15,33 @@ class Solution {
 //         }
 //         int temp=(int)ans;
 //        return isNegative?temp*-1:temp;
-       long res =0;
         
         
-        while(x!=0){
-            res = res*10 + x%10;
-            x=x/10;
-        }
-        if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE)
+        // second way
+        
+//        long res =0;
+        
+        
+//         while(x!=0){
+//             res = res*10 + x%10;
+//             x=x/10;
+//         }
+//         if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE)
+//             return 0;
+        
+//             return (int)res;
+        
+        long res=0;
+         while(x!=0)
+         {
+             res=res*10+x%10;
+             x/=10;
+         }
+    
+         if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE)
             return 0;
         
-            return (int)res;
+        return (int)res;
         
     }
 }
