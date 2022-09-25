046c7134c2f1a0630a432df13ac20e4a57f633f6
Ashu Baghel <102874744+ashu2206@users.noreply.github.com>
2022-09-25 13:58:34 +0530

    Time: 3 ms (49.27%), Space: 41.9 MB (11.53%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,21 +1,23 @@
 class Solution {
     public boolean isHappy(int n) {
-   ArrayList list=new ArrayList();
-        while(n!=1)
-        {
-            int temp=n;
-            n=0;
-            while(temp>0)
-            {
-                n=n+(int)Math.pow((temp%10),2);
-                temp/=10;
-            }
-            if(list.contains(n))
-               return false;
-            else
-                list.add(n);
-        }
-        return true;
+  ArrayList list= new ArrayList();
+   while(n!=1)
+   {
+       int temp=n;
+       n=0;
+       while(temp>0)
+       {
+           n+=(int)Math.pow((temp%10),2);
+           temp/=10;
+       }
+       if(list.contains(n))
+          return false;
+        else  
+           list.add(n);
+       
+           
+   }
+               return true;
 
 }
 }
